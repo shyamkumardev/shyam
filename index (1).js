4649529c9bfac122c5b49d0b5be78a358a0e6572
shyamkumardev <funrhymetv@gmail.com>
2026-05-06 12:35:02 +0530

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,48 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+
+console.log("Bot starting...");
+
+// ✅ ENV variables use karo
+const token = process.env.BOT_TOKEN;
+const API_KEY = process.env.API_KEY;
+
+const bot = new TelegramBot(token, { polling: true });
+
+const channel = "@Dnexon55Pros";
+const API_URL = "https://telekartsmm.com/api/v2";
+
+// ✅ Duplicate protection
+let processedPosts = new Set();
+
+bot.on("channel_post", async (msg) => {
+  try {
+    if (processedPosts.has(msg.message_id)) return;
+    processedPosts.add(msg.message_id);
+
+    const postLink = `https://t.me/${channel.replace("@","")}/${msg.message_id}`;
+
+    console.log("🚀 New Post:", postLink);
+
+    const delay = 1000 + Math.floor(Math.random() * 3000);
+
+    setTimeout(async () => {
+
+      console.log("📤𝕻𝖑𝖆𝖈𝖎𝖓𝖌 𝕺𝖗𝖉𝖊𝖗...");
+
+      await axios.post(API_URL, {
+        key: API_KEY,
+        action: "add",
+        service: "1023",
+        link: postLink,
+        quantity: 110
+      });
+
+      console.log("✅ 𝓞𝓻𝓭𝓮𝓻 𝓢𝓾𝓬𝓬𝓮𝓼𝓼𝓯𝓾𝓵𝓵");
+
+    }, delay);
+
+  } catch (err) {
+    console.log("❌ Error:", err.message);
+  }
+});
